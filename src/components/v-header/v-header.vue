3f14ae75d6d27b4f5185f8eeb12b28bcc5688c12
shijue1 <shengqiputixin@aliyun.com>
2019-12-18 19:34:36 +0800

    up

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -9,11 +9,16 @@
       </div>
       <div class="content">
         <div class="title">
-          <span class="brand">123</span>
+          <span class="brand"></span>
           <span class="name">123</span>
         </div>
         <div class="description">{{2}}/{{3}}分钟送达</div>
       </div>
+
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{1}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
 </template>
@@ -28,9 +33,58 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
 @import '~common/stylus/mixin';
 @import '~common/stylus/variable';
+
 .header {
-  position:relative;
-  overflow:hidden;
-  color:$color-white;
+  position: relative;
+  overflow: hidden;
+  color: $color-white;
+
+  .content-wrapper {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 24px 12px 18px 24px;
+
+    .avatar {
+      flex: 0 0 64px;
+      width: 64px;
+      margin-right: 16px;
+
+      img {
+        border-radius: 2px;
+      }
+    }
+
+    .content {
+      flex: 1;
+      color: red;
+
+      .title {
+        display: flex;
+        align-items: center;
+        margin-bottom: 8px;
+
+        .brand {
+          width: 30px;
+          height: 18px;
+          bg-image('brand');
+          background-size: 30px 18px;
+          background-repeat: no-repeat;
+        }
+
+        .name {
+          margin-left: 6px;
+          font-size: $fontsize-large;
+          font-weight: bold;
+        }
+      }
+
+      .description {
+        margin-bottom: 8px;
+        line-height: 12px;
+        font-size: $fontsize-small;
+      }
+    }
+  }
 }
 </style>
