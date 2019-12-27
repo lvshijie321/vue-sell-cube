186d19a42246e18a226eec104b1d7ba96f9d4d23
shijue1 <shengqiputixin@aliyun.com>
2019-12-27 20:03:17 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <v-header :seller="seller" />
-    <!-- <div class="tab-wrapper"> -->
-    <tab :tabs="tabs"></tab>
-    <!-- </div> -->
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
@@ -11,6 +11,9 @@
 import VHeader from './components/v-header/v-header.vue'
 import Tab from 'components/tab/tab'
 import { getSeller } from 'api'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
 export default {
   name: 'app',
   components: {
@@ -27,21 +30,21 @@ export default {
       return [
         {
           label: '商品',
-          // component: Goods,
+          component: Goods,
           data: {
             seller: this.seller
           }
         },
         {
           label: '评论',
-          // component: Ratings,
+          component: Ratings,
           data: {
             seller: this.seller
           }
         },
         {
           label: '商家',
-          // component: Seller,
+          component: Seller,
           data: {
             seller: this.seller
           }
@@ -64,12 +67,11 @@ export default {
 </script>
 
 <style lang="stylus">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app
+  .tab-wrapper
+      position: fixed
+      top: 137px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="tab">
-    <cube-tab-bar
-      :useTransition=false
-      :showSlider=true
-      v-model="selectedLabel"
-      :data="tabs"
-      ref="tabBar"
-      class="border-bottom-1px"
-    >
+    <cube-tab-bar :useTransition=false
+                  :showSlider=true
+                  v-model="selectedLabel"
+                  :data="tabs"
+                  ref="tabBar">
     </cube-tab-bar>
     <div class="slide-wrapper">
-      <cube-slide
-        :loop=false
-        :auto-play=false
-        :show-dots=false
-        :initial-index="index"
-        ref="slide"
-        :options="slideOptions"
-        @scroll="onScroll"
-        @change="onChange"
-      >
-        <cube-slide-item v-for="(tab,index) in tabs" :key="index">
-          <component ref="component" :is="tab.component" :data="tab.data"></component>
+      <cube-slide :loop=false
+                  :auto-play=false
+                  :show-dots=false
+                  :initial-index="index"
+                  ref="slide"
+                  :options="slideOptions"
+                  @scroll="onScroll"
+                  @change="onChange">
+        <cube-slide-item v-for="(tab, index) in tabs"
+                         :key="index">
+          <component ref="component"
+                     :is="tab.component"
+                     :data="tab.data" />
         </cube-slide-item>
       </cube-slide>
     </div>
@@ -29,73 +27,77 @@
 </template>
 
 <script>
-  export default {
-    name: 'tab',
-    props: {
-      tabs: {
-        type: Array,
-        default() {
-          return []
-        }
-      },
-      initialIndex: {
-        type: Number,
-        default: 0
+export default {
+  name: 'tab',
+  props: {
+    tabs: {
+      type: Array,
+      default() {
+        return []
       }
     },
-    data() {
-      return {
-        index: this.initialIndex,
-        slideOptions: {
-          listenScroll: true,
-          probeType: 3,
-          directionLockThreshold: 0
-        }
-      }
-    },
-    computed: {
-      selectedLabel: {
-        get() {
-          return this.tabs[this.index].label
-        },
-        set(newVal) {
-          this.index = this.tabs.findIndex((value) => {
-            return value.label === newVal
-          })
-        }
-      }
-    },
-    mounted() {
-      this.onChange(this.index)
-    },
-    methods: {
-      onScroll(pos) {
-        const tabBarWidth = this.$refs.tabBar.$el.clientWidth
-        const slideWidth = this.$refs.slide.slide.scrollerWidth
-        const transform = -pos.x / slideWidth * tabBarWidth
-        this.$refs.tabBar.setSliderTransform(transform)
-      },
-      onChange(current) {
-        this.index = current
-        const instance = this.$refs.component[current]
-        if (instance && instance.fetch) {
-          instance.fetch()
-        }
+    initialIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      index: this.initialIndex,
+      // 为了实施派发滚动距离（onScroll 被回调）需要添加，listenScroll、probeType
+      slideOptions: {
+        listenScroll: true,
+        probeType: 3,
+        directionLockThreshold: 0 // 同时竖向滚动和横向滚动，添加阈值
       }
     }
+  },
+  methods: {
+    onScroll(pos) {
+        const tabBarWidth = this.$refs.tabBar.$el.clientWidth // tabbar 完整的 width
+        const slideWidth = this.$refs.slide.slide.scrollerWidth // n 个 tab 页 width 总和
+        const transform = -pos.x / slideWidth * tabBarWidth
+        this.$refs.tabBar.setSliderTransform(transform)
+    },
+    onChange(current) {
+      this.index = current
+      const component = this.$refs.component[current]
+      component.fetch && component.fetch()
+    }
+  },
+  computed: {
+    selectedLabel: {
+      get() {
+        return this.tabs[this.index].label
+      },
+      set(newVal) {
+        this.index = this.tabs.findIndex(value => {
+          return value.label === newVal
+        })
+      }
+    }
+  },
+  mounted() {
+    this.onChange(this.index)
   }
+}
 </script>
 
 <style lang="stylus" scoped>
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .tab
-    display: flex
-    flex-direction: column
-    height: 100%
-    >>> .cube-tab
-      padding: 10px 0
-    .slide-wrapper
-      flex: 1
-      overflow: hidden
+.tab {
+  display: flex;
+  flex-direction: column;
+  height: 100%; // @todo
+
+  >>> .cube-tab {
+    padding: 10px 0;
+  }
+
+  .slide-wrapper {
+    flex: 1;
+    overflow: hidden;
+  }
+}
 </style>
